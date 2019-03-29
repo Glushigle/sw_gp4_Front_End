ff407d050906c107e3a0bf9581e25d11844f1480
Glushigle <2456898189@qq.com>
2019-03-29 22:54:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sw_gp4/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.sw_gp4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View v) {
+        Toast.makeText(this, "helloworld", Toast.LENGTH_SHORT).show();
+
+    }
+
+}
