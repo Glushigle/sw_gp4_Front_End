060275a3a302b787ff18f4f9a0ca5cd6aef994bf
Kate Hsu <shanglinghsu@gmail.com>
2019-04-26 01:33:27 +0800

    Add POST for group_list

--- app/src/main/java/com/example/sw_gp4/GroupList.java
+++ app/src/main/java/com/example/sw_gp4/GroupList.java
@@ -7,6 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.OutputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+
 public class GroupList extends AppCompatActivity {
 
     private TextView mTextMessage;
@@ -30,6 +36,39 @@ public class GroupList extends AppCompatActivity {
             return false;
         }
     };
+
+    /** "/** Enter" generates the template for function description. **/
+    /**
+     * Post user_id to obtain the user's group_list.
+     * Intended to be modified for other POST requests.
+     * TODO Ask for proper API from backend.
+     * https://www.wikihow.com/Execute-HTTP-POST-Requests-in-Android
+     * @param user_id User requesting group_list    TODO How to obtain user_id locally?
+     * @return group_list in String
+     */
+    private String getGroupList(String user_id) {
+        URL url = new URL("full_url");
+        HttpURLConnection client = null;
+        try {
+            client = (HttpURLConnection) url.openConnection();
+            client.setRequestMethod("POST");
+            client.setRequestProperty("user_id",user_id);
+            client.setDoOutput(true);
+
+            OutputStream outputPost = new BufferedOutputStream(client.getOutputStream());
+            writeStream(outputPost);
+            outputPost.flush();
+            outputPost.close();
+
+        } catch (Exception e){
+            // TODO handle exception
+            e.printStackTrace(); // Bad practice!
+            return "";
+        } finally {
+            if(client != null) // Make sure the connection is not null.
+                client.disconnect();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
