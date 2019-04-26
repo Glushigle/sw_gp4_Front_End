1296405390dcaecf9fdc1b7a5f96b0c59ffada48
Kate Hsu <shanglinghsu@gmail.com>
2019-04-26 21:57:48 +0800

    Add swipe-to-delete code

--- app/src/main/java/com/example/sw_gp4/GroupList.java
+++ app/src/main/java/com/example/sw_gp4/GroupList.java
@@ -51,6 +51,51 @@ public class GroupList extends AppCompatActivity {
                 LinearLayout.LayoutParams.WRAP_CONTENT));
 
         myLayout.addView(myButton);
+
+        //TODO: adapter rather than this naive method
+    }
+
+    // from https://github.com/daimajia/AndroidSwipeLayout
+    private void addSwipeLayout(){
+        SwipeLayout swipeLayout =  (SwipeLayout)findViewById(R.id.sample1);
+
+        //set show mode.
+        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+
+        //add drag edge.(If the BottomView has 'layout_gravity' attribute, this line is unnecessary)
+        swipeLayout.addDrag(SwipeLayout.DragEdge.Left, findViewById(R.id.bottom_wrapper));
+
+        swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onClose(SwipeLayout layout) {
+                //when the SurfaceView totally cover the BottomView.
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+                //you are swiping.
+            }
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                //when the BottomView totally show.
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                //when user's hand released.
+            }
+        });
     }
 
     private void renderGroupListView(){ // String response
