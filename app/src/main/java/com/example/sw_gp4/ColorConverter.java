e571e7f4bec8cb52bd9992838d6ca94504bc6ce2
Kate Hsu <shanglinghsu@gmail.com>
2019-05-27 20:18:15 +0800

    Add username converter to color converter

--- app/src/main/java/com/example/sw_gp4/ColorConverter.java
+++ app/src/main/java/com/example/sw_gp4/ColorConverter.java
@@ -2,8 +2,10 @@ package com.example.sw_gp4;
 
 import android.graphics.Color;
 
+import java.util.Random;
+
 public class ColorConverter {
-    public static int id2color(String id_str){
+    public static int id2color(int id){
         final int A = 201;
         final int B = 237;
             /* RGB: 1个255; 1个178; 1个178<x<255
@@ -11,7 +13,6 @@ public class ColorConverter {
                 id%2 得剩下2个位置里178的前后
                 id/6%77 map进[178, 255]间
             */
-        int id = Integer.parseInt(id_str);
         int[] RGB = {-1,-1,-1};
 
         RGB[id%3] = B;
@@ -20,5 +21,20 @@ public class ColorConverter {
         for(int i=0; i<3; ++i){ if(RGB[i]<0) RGB[i] = A+(id/6)%(B-A); }
 
         return Color.argb(255, RGB[0], RGB[1], RGB[2]);
+    }
+    public static int id2color(String id_str){
+        return id2color(Integer.parseInt(id_str));
+    }
+    public static int username2color(String username){
+        int fake_id = 0;
+        Random random = new Random();
+        for(char c:username.toCharArray()){
+            try{
+                fake_id += ((int)c)*19;
+            } catch (Exception e) {
+                fake_id += (random.nextInt(127));
+            }
+        }
+        return id2color(fake_id);
     }
 }
