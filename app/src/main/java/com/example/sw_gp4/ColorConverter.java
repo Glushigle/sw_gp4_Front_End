3d8864c60c0922a509be4ab39d75f0affb6f54de
Kate Hsu <shanglinghsu@gmail.com>
2019-06-02 15:26:41 +0800

    Adjust color round

--- app/src/main/java/com/example/sw_gp4/ColorConverter.java
+++ app/src/main/java/com/example/sw_gp4/ColorConverter.java
@@ -10,14 +10,14 @@ public class ColorConverter {
     /* RGB: 1个B; 1个A; 1个A<x<B
     =>  id%3 得B的位置,
         id%2 得剩下2个位置里A的前后
-        id/6%77 map进[A, B]间
+        id/6 map进[A, B]间: 中左右
     */
     public static int fromId(int id){
         int[] RGB = {-1,-1,-1};
         RGB[id%3] = B;
         if(RGB[id%2]<0) RGB[id%2] = A;
         else RGB[id%2+1] = A;
-        for(int i=0; i<3; ++i){ if(RGB[i]<0) RGB[i] = A+(id/6)%(B-A); }
+        for(int i=0; i<3; ++i){ if(RGB[i]<0) RGB[i] = A+id*13%(B-A); }
 
         return Color.argb(255, RGB[0], RGB[1], RGB[2]);
     }
